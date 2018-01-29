40a90b00e2c9f58c50b61d155c152e3d4df2717c
Mark Prediger <markprediger@gmail.com>
2018-01-29 19:47:01 +0100

    [tiny_reconfigure] Add refresh button to parameters and groups in gui

--- tiny_reconfigure/scripts/test_server.py
+++ tiny_reconfigure/scripts/test_server.py
@@ -3,21 +3,94 @@
 #from tiny_reconfigure.srv import *
 from tiny_reconfigure.msg import *
 import rospy
+import random
 
 groupPub = None
+paramPub = None
 
 def handle_get_def(req):
-    print "handle_get_def"
-    global groupPub
+    global groupPub, paramPub
 
-    groupPub.publish(GroupDef(1, 2, "group 1"))
-    groupPub.publish(GroupDef(0, 2, "group 0"))
+    groupDefs = ["group 0", "group 1"]
+    
+    paramDefs = [[["param_0_0", ParameterDef.TYPE_INT]],
+                 [["param_1_0", ParameterDef.TYPE_FLOAT],
+                  ["param_1_1", ParameterDef.TYPE_INT],
+                  ["param_1_2", ParameterDef.TYPE_FLOAT]]]
+    
+    if req.cmd == GetDef.CMD_LIST_ALL:
+        print "handle_get_def CMD_LIST_ALL"
+        
+        groupPub.publish(GroupDef(1, 2, "group 1"))
+        groupPub.publish(GroupDef(0, 2, "group 0"))
+    
+        if bool(random.getrandbits(1)):
+            paramPub.publish(ParameterDef(group_id = 0,
+                                          param_id = 0,
+                                          num_params = 1,
+                                          param_name = "param_0_0",
+                                          type = ParameterDef.TYPE_INT,
+                                          value_int = 42,
+                                          value_float = 0))
+    
+        if bool(random.getrandbits(1)):
+            paramPub.publish(ParameterDef(group_id=1,
+                                          param_id=0,
+                                          num_params=3,
+                                          param_name="param_1_0",
+                                          type=ParameterDef.TYPE_FLOAT,
+                                          value_int=0,
+                                          value_float=0.123))
+    
+        if bool(random.getrandbits(1)):
+            paramPub.publish(ParameterDef(group_id=1,
+                                          param_id=1,
+                                          num_params=3,
+                                          param_name="param_1_1",
+                                          type=ParameterDef.TYPE_INT,
+                                          value_int=13,
+                                          value_float=0))
+    
+        if bool(random.getrandbits(1)):
+            paramPub.publish(ParameterDef(group_id=1,
+                                          param_id=2,
+                                          num_params=3,
+                                          param_name="param_1_2",
+                                          type=ParameterDef.TYPE_FLOAT,
+                                          value_int=0,
+                                          value_float=2.34))
+            
+    elif req.cmd == GetDef.CMD_GET_GROUP_DEF:
+        print "handle_get_def CMD_GET_GROUP_DEF " + str(req.group_id)
+        groupPub.publish(GroupDef(group_id=req.group_id,
+                                  num_groups=len(groupDefs),
+                                  group_name=groupDefs[req.group_id]))
+        
+        for param_id in xrange(len(paramDefs[req.group_id])):
+            paramPub.publish(ParameterDef(group_id=req.group_id,
+                                          param_id=param_id,
+                                          num_params=len(paramDefs[req.group_id]),
+                                          param_name=paramDefs[req.group_id][param_id][0],
+                                          type=paramDefs[req.group_id][param_id][1],
+                                          value_int=23,
+                                          value_float=2.34))
+    elif req.cmd == GetDef.CMD_GET_PARAM_DEF:
+        print "handle_get_def CMD_GET_PARAM_DEF " + str(req.group_id) + " " + str(req.param_id)
+        paramPub.publish(ParameterDef(group_id=req.group_id,
+                                      param_id=req.param_id,
+                                      num_params=len(paramDefs[req.group_id]),
+                                      param_name=paramDefs[req.group_id][req.param_id][0],
+                                      type=paramDefs[req.group_id][req.param_id][1],
+                                      value_int=23,
+                                      value_float=2.34))
+        
 
 def add_two_ints_server():
     rospy.init_node('add_two_ints_server')
     s = rospy.Subscriber('get_def', GetDef, handle_get_def)
-    global groupPub
+    global groupPub, paramPub
     groupPub = rospy.Publisher('group_def', GroupDef, queue_size=10)
+    paramPub = rospy.Publisher('param_def', ParameterDef, queue_size=10)
     print "Ready to add two ints."
     rospy.spin()
 
--- tiny_reconfigure/src/tiny_reconfigure/tiny_ref_gui.py
+++ tiny_reconfigure/src/tiny_reconfigure/tiny_ref_gui.py
@@ -55,13 +55,10 @@ class ReconfigureWidget(QWidget):
         rospy.sleep(0.5)
         #try:
         #   self.pubGetParamDef.wait_for_service(1)
-        self.pubGetParamDef.publish(tiny_reconfigure.msg.GetDef()) # tiny_reconfigure.msg.GetDef.CMD_LIST_ALL, 0, 0
+        self.pubGetParamDef.publish(tiny_reconfigure.msg.GetDef(tiny_reconfigure.msg.GetDef.CMD_LIST_ALL, 0, 0))
         #   print "srvGetParamDef returned"
         #except (rospy.ServiceException, rospy.ROSException) as ex:
         #    print("Service did not process request: " + str(ex))
-
-    def onSendPressed(self, group, item):
-        print "pressed group:%s item:%s value:%i" % (group.text(0), item.name, item.value)
 
     def onGroupDefReceived(self, msg):
         print "onGroupDefReceived"
@@ -77,8 +74,10 @@ class ReconfigureWidget(QWidget):
     
     def onParameterDefReceivedQt(self, msg):
         group = self.getCreateGroupItem(msg.group_id)
-        item = ParameterTreeItem(self, group, msg.param_name, "type")
-        item.button.clicked.connect(lambda: self.onSendPressed(group, item))
+        if not group is None:
+            item = self.getCreateParamItem(group, msg)
+            item.setText(0, msg.param_name)
+            item.setText(1, "type")
 
     def getCreateGroupItem(self, index, num = None):
         if not num is None:
@@ -86,22 +85,61 @@ class ReconfigureWidget(QWidget):
                 self.parameterTree.takeTopLevelItem(self.parameterTree.topLevelItemCount() - 1)
         
             while self.parameterTree.topLevelItemCount() < num:
-                group = QTreeWidgetItem(self.parameterTree)
+                group = GroupTreeItem(self.parameterTree, "<undefined>")
                 self.parameterTree.addTopLevelItem(group)
+                self.parameterTree.expandItem(group)
+                group.btnRefresh.clicked.connect(lambda clicked, group=group: self.onRefreshGroupPressed(group))
 
         return self.parameterTree.topLevelItem(index)
 
-    def getCreateParamItem(self, group, index, num=None):
-        if not num is None:
-            while group.childCount() > num:
+    def getCreateParamItem(self, group, msg):
+        if not msg.num_params is None:
+            while group.childCount() > msg.num_params:
                 group.takeChild(group.childCount() - 1)
 
-            if group.childCount() < num:
-                group = ParameterTreeItem(self)
-                self.parameterTree.insertTopLevelItem(index, group)
-                return group
+            while group.childCount() < msg.num_params:
+                item = ParameterTreeItem(group, "<undefined>", "<none>")
+                item.btnSend.clicked.connect(lambda clicked, item=item, group=group: self.onSendPressed(group, item))
+                item.btnRefresh.clicked.connect(lambda clicked, item=item, group=group: self.onRefreshParamPressed(group, item))
 
-        return self.parameterTree.topLevelItem(index)
+        return group.child(msg.param_id)
+    
+    def onRefreshGroupPressed(self, group):
+        self.pubGetParamDef.publish(tiny_reconfigure.msg.GetDef(tiny_reconfigure.msg.GetDef.CMD_GET_GROUP_DEF,
+                                                                self.parameterTree.indexOfTopLevelItem(group),
+                                                                0))
+    
+    def onSendPressed(self, group, item):
+        print "send group:%s item:%s value:%i" % (group.text(0), item.name, item.value)
+    
+    def onRefreshParamPressed(self, group, item):
+        self.pubGetParamDef.publish(tiny_reconfigure.msg.GetDef(tiny_reconfigure.msg.GetDef.CMD_GET_PARAM_DEF, self.parameterTree.indexOfTopLevelItem(group), group.indexOfChild(item)))
+
+# ------------------------------------------------------------------------------
+# Custom QTreeWidgetItem
+# ------------------------------------------------------------------------------
+class GroupTreeItem(QTreeWidgetItem):
+    '''
+    Custom QTreeWidgetItem with Widgets
+    '''
+
+    def __init__(self, parent, name):
+        '''
+        parent (QTreeWidget) : Item's QTreeWidget parent.
+        name   (str)         : Item's name. just an example.
+        '''
+
+        ## Init super class ( QTreeWidgetItem )
+        super(GroupTreeItem, self).__init__(parent)
+
+        ## Column 0 - name:
+        self.setText(0, name)
+        
+        ## Column 1 - Refresh Button:
+        self.btnRefresh = QPushButton()
+        self.btnRefresh.setText("Refresh")
+        self.treeWidget().setItemWidget(self, 1, self.btnRefresh)
+
 
 
 # ------------------------------------------------------------------------------
@@ -132,10 +170,15 @@ class ParameterTreeItem(QTreeWidgetItem):
         self.spinBox.setValue(0)
         self.treeWidget().setItemWidget(self, 2, self.spinBox)
 
-        ## Column 3 - Button:
-        self.button = QPushButton()
-        self.button.setText("Send")
-        self.treeWidget().setItemWidget(self, 3, self.button)
+        ## Column 3 - Send Button:
+        self.btnSend = QPushButton()
+        self.btnSend.setText("Send")
+        self.treeWidget().setItemWidget(self, 3, self.btnSend)
+
+        ## Column 4 - Refresh Button:
+        self.btnRefresh = QPushButton()
+        self.btnRefresh.setText("Refresh")
+        self.treeWidget().setItemWidget(self, 4, self.btnRefresh)
 
         ## Signals
         # self.treeWidget().connect( self.button, SIGNAL("clicked()"), self.buttonPressed )
@@ -153,12 +196,3 @@ class ParameterTreeItem(QTreeWidgetItem):
         Return value ( 2nd column int)
         '''
         return self.spinBox.value()
-
-    def buttonPressed(self):
-        '''
-        Triggered when Item's button pressed.
-        an example of using the Item's own values.
-        '''
-        # print "This Item name:%s value:%i" %( self.name,
-        #                                      self.value )
-        # self.reconfWidget.onSendPressed(self.parent, self)
