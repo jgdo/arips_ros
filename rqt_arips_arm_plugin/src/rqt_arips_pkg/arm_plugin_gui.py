92ac357e2f1706e9250759117aaeeaa23669e99d
Mark Prediger <markprediger@gmail.com>
2018-03-02 21:54:34 +0100

    [rqt_arips_arm_plugin] Basic implementation for Raw and Direct Control modes

--- rqt_arips_arm_plugin/src/rqt_arips_pkg/arm_plugin_gui.py
+++ rqt_arips_arm_plugin/src/rqt_arips_pkg/arm_plugin_gui.py
@@ -32,11 +32,16 @@ class ArmGuiWidget(QWidget):
         self.setObjectName('ArmPluginUi')
         
         self.btnConnectDisconnect.clicked.connect(self.onConnectPressed)
+        self.btnModeRawMotors.clicked.connect(self.onModeRawPressed)
+        self.btnSendSetpoint.clicked.connect(self.onSendSetpointsPressed)
+        self.btnModeController.clicked.connect(self.onModeControllerPressed)
         self.sigMotionStateReceived.connect(self.onMotionStateReceivedQt)
 
         self.connectionStatus = self.CSTATE_DISCONNECTED
 
         self.subMotionState = None
+        self.pubMotionCommand = None
+        self.pubRawMotor = None
 
     def shutdown(self):
         self.disconnect()
@@ -49,21 +54,35 @@ class ArmGuiWidget(QWidget):
 
     def connect(self):
         self.disconnect()
-        self.subMotionState = rospy.Subscriber(self.prefixEdit.text() + "motion_state", arips_arm_msgs.msg.MotionState,
+
+        currentTopicRoot = self.prefixEdit.text()
+
+        self.subMotionState = rospy.Subscriber(currentTopicRoot + "/motion_state", arips_arm_msgs.msg.MotionState,
                                                self.onMotionStateReceived, queue_size=10)
+        self.pubMotionCommand = rospy.Publisher(currentTopicRoot + "/motion_command", arips_arm_msgs.msg.MotionCommand,
+                                              queue_size=10)
+        self.pubRawMotor = rospy.Publisher(currentTopicRoot + "/raw_motor_command", arips_arm_msgs.msg.RawMotorCommand,
+                                                queue_size=10)
 
         self.connectionStatus = self.CSTATE_CONNECTED
         self.lblStatus.setText("Connecting ...")
         self.btnConnectDisconnect.setText("Disconnect")
 
     def disconnect(self):
-        if not self.subMotionState is None:
+        if self.isConnected():
             self.subMotionState.unregister()
             self.subMotionState = None
+            self.pubMotionCommand.unregister()
+            self.pubMotionCommand = None
+            self.pubRawMotor.unregister()
+            self.pubRawMotor = None
 
         self.connectionStatus = self.CSTATE_DISCONNECTED
         self.lblStatus.setText("Disconnected")
         self.btnConnectDisconnect.setText("Connect")
+
+    def isConnected(self):
+        return self.connectionStatus == self.CSTATE_CONNECTED
 
     def onMotionStateReceived(self, msg):
         self.sigMotionStateReceived.emit(msg)
@@ -75,12 +94,7 @@ class ArmGuiWidget(QWidget):
             self.tblJointStates.item(row, col).setText(text)
 
     def onMotionStateReceivedQt(self, msg):
-        #print "onMotionStateReceivedQt"
-        # print self.tblJointStates.item(0, 0).setText("bla")
-        #self.tblJointStates.item(1, 0).setText("bla")
-        #self.tblJointStates.item(2, 0).setText("bla")
-
-        #self.tblJointStates.setItem(0, 1, QTableWidgetItem("Hello"))
+        self.lblStatus.setText(self.statusText(msg.mode))
 
         for i in range(0, len(msg.jointStates)):
             # print "row: " + str(i) + " col: " + str(self.JINDEX_POS)
@@ -88,3 +102,44 @@ class ArmGuiWidget(QWidget):
             self.setJointItemText(i, self.JINDEX_VEL, str(math.degrees(msg.jointStates[i].velocity)))
             self.setJointItemText(i, self.JINDEX_GOAL_POS, str(math.degrees(msg.jointStates[i].setpoint_pos)))
             self.setJointItemText(i, self.JINDEX_GOAL_VEL, str(math.degrees(msg.jointStates[i].setpoint_vel)))
+
+    def statusText(self, mode):
+        stateDict = {
+            arips_arm_msgs.msg.MotionState.M_IDLE: "Idle",
+            arips_arm_msgs.msg.MotionState.M_BREAK: "Break",
+            arips_arm_msgs.msg.MotionState.M_HOLD: "Hold",
+            arips_arm_msgs.msg.MotionState.M_RAW_MOTORS: "Raw Motors",
+            arips_arm_msgs.msg.MotionState.M_DIRECT_JOINTS: "Direct Effort",
+            arips_arm_msgs.msg.MotionState.M_DIRECT_CONTROLLER: "Direct Controller",
+            arips_arm_msgs.msg.MotionState.M_TRAJECTORY: "Trajectory"
+        }
+
+        state = "Unknown"
+        if mode in stateDict:
+            state = stateDict[mode]
+
+        return "Connected: {} ({})".format(state, mode)
+
+    def onModeRawPressed(self):
+        if self.isConnected():
+            msg = arips_arm_msgs.msg.MotionCommand()
+            msg.command = arips_arm_msgs.msg.MotionCommand.CMD_RAW_MOTORS
+            self.pubMotionCommand.publish(msg)
+
+    def onSendSetpointsPressed(self):
+        if self.isConnected():
+            msg = arips_arm_msgs.msg.RawMotorCommand()
+            for i in range(len(msg.raw_motor_power)):
+                try:
+                    msg.raw_motor_power[i] = float(self.tblJointStates.item(i, self.JINDEX_SETPOINT).text())
+                except ValueError:
+                    msg.raw_motor_power[i] = 0.0
+                self.tblJointStates.item(i, self.JINDEX_SETPOINT).setText(str(msg.raw_motor_power[i]))
+
+            self.pubRawMotor.publish(msg)
+
+    def onModeControllerPressed(self):
+        if self.isConnected():
+            msg = arips_arm_msgs.msg.MotionCommand()
+            msg.command = arips_arm_msgs.msg.MotionCommand.CMD_DIRECT_CONTROLLER
+            self.pubMotionCommand.publish(msg)
