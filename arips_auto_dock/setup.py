d610f48ec2b845e666999f4ce3aee16a19cdbb25
Mark Prediger <markprediger@gmail.com>
2020-10-16 15:51:35 +0200

    updated projects for proper catkin install, added face detector launch

--- arips_auto_dock/setup.py
+++ arips_auto_dock/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['arips_auto_dock'],
-    scripts=['scripts'],
+    scripts=['scripts/auto_dock'],
     package_dir={'': 'src'}
 )
 
--- arips_user_interaction/setup.py
+++ arips_user_interaction/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    scripts=['scripts'],
+    scripts=['scripts/user_interaction_node'],
     packages=['arips_user_interaction'],
     package_dir={'': 'src'},
 )
